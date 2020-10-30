6021244b5d8d8cbb44cd018571e59348092ddefd
zhouyc <zhouyc@ist.com.hk>
2020-10-30 19:16:20 +0800

    提交漏传文件

---
+++ fishtoucher/src/main/java/com/ist/fishtoucher/constant/GlobalConstant.java
@@ -0,0 +1,13 @@
+package com.ist.fishtoucher.constant;
+
+public class GlobalConstant {
+    private static boolean FISH_MODE = true;
+
+    public static boolean isFishMode() {
+        return FISH_MODE;
+    }
+
+    public static void setFishMode(boolean isFishMode) {
+        FISH_MODE = isFishMode;
+    }
+}
