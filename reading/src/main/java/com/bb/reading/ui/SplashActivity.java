5b10de3eea01daaf90de7481be5e27a27ce1feb8
Boby <1135318667@qq.com>
2021-04-18 02:10:46 +0800

    [SplashActivity]Debug下点击快速进入主页

--- reading/src/main/java/com/bb/reading/ui/SplashActivity.java
+++ reading/src/main/java/com/bb/reading/ui/SplashActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 import com.bb.module_common.utils.StatusBarUtils;
+import com.bb.reading.BuildConfig;
 import com.bb.reading.R;
 import com.bb.reading.ui.mvp.view.activity.MainActivity;
 
@@ -21,17 +23,39 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         StatusBarUtils.setStatusBar(this, Color.TRANSPARENT,true,true);
 
+        initFastEnterHomeWay();
         countDownToMainActivity();
     }
 
+    /**
+     * 点击快速进入主页
+     */
+    private void initFastEnterHomeWay() {
+        if (BuildConfig.DEBUG) {
+            findViewById(R.id.flash_text_logo).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mHandler.removeCallbacks(mToMainPageRunnable);
+                    jumpToMainPage();
+                }
+            });
+        }
+    }
+
     private void countDownToMainActivity() {
-        mHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, 2900);
+        mHandler.postDelayed(mToMainPageRunnable, 2900);
+    }
+
+    private Runnable mToMainPageRunnable = new Runnable() {
+        @Override
+        public void run() {
+            jumpToMainPage();
+        }
+    };
+
+    private void jumpToMainPage() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
